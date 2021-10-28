9ea04d2839c4f5f8e17f69def33c96b54f236382
arielkru <63583491+arielkru@users.noreply.github.com>
2021-10-28 18:57:07 +0300

    Fix BC_AWS_GENERAL_56
    Co-authored-by: bridgecrew-staging[bot] <83755608+bridgecrew-staging[bot]@users.noreply.github.com>

--- test.tf
+++ test.tf
@@ -19,4 +19,11 @@ resource "aws_s3_bucket" "test1" {
   versioning {
     enabled = true
   }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "aws:kms"
+      }
+    }
+  }
 }
